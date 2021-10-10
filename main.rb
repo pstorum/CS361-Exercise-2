5e90249efe5e7c69129ea2181425ff56680972ca
Peter Storum <p-storum@hotmail.com>
2021-10-10 11:57:20 -0700

    Update main.rb
    removed the global variable, and utilized the ruby functionality that the last line of the function is automatically returned

--- main.rb
+++ main.rb
@@ -1,15 +1,14 @@
 # Name Mangler
-
-@name = "Johanna Jackson"
-def reverse_name()
-  return @name = @name.split.reverse.join(' ')
+def reverse_name(name)
+  name.split.reverse.join(' ')
 end
-def append_borg_to_name()
-  return @name = @name << " Borg"
+def append_borg_to_name(name)
+  name << " Borg"
 end
-puts "Current Name: #{@name}"
-puts "New name: #{reverse_name()}"
-puts "New borg name: #{append_borg_to_name()}"
+name = "Johanna Jackson"
+puts "Current Name: #{name}"
+puts "New name: #{name = reverse_name(name)}"
+puts "New borg name: #{name = append_borg_to_name(name)}"
 
 
 #comment as a student. I felt like the names of the functions were pretty clear as to what they did and any comments would have been redundent.
